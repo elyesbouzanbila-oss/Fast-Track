90a05f8fc91ad7ad06cafc0093178b84a2527877
elyesbouzanbila-oss <elyesbouzanbila@gmail.com>
2026-04-30 20:16:35 +0100

    Clear repository for fresh start

--- Frontend/img.js
+++
@@ -1,7 +0,0 @@
-const logo = document.querySelector('.logo');
-
-logo.style.width = '140px';
-logo.style.height = 'auto';
-logo.style.marginTop = '-210px';
-logo.style.marginLeft = '400px';
-logo.style.marginBottom = '-20px';
